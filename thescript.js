4d507928fa03a575a5de056b5442c463cb5ca860
eric.s.w.broome.secure@gmail.com <eric.s.w.broome.secure@gmail.com>
2021-05-22 11:40:13 -0500

    Fixed title element

--- thescript.js
+++ thescript.js
@@ -1,91 +1,48 @@
-/* Object destructuring 1 */
-const objectDestructuring_1 = function() {
-    let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
-    let {numPlanets, yearNeptuneDiscovered} = facts;
-    console.log(numPlanets);
-    console.log(yearNeptuneDiscovered);
-    return [
-        numPlanets, //8
-        yearNeptuneDiscovered //1846
-    ]; 
+const question_1 = function() {
+    const set = new Set([1, 1, 2, 2, 3, 4]);
+    return Array.from(set);
+    //(4) [1, 2, 3, 4]
 }
 
-/* Object destructuring 2 */
-const objectDestructuring_2 = function() {
-    let planetFacts = {
-        numPlanets: 8,
-        yearNeptuneDiscovered: 1846,
-        yearMarsDiscovered: 1659
-    };
-    let {numPlanets, ...discoveryYears} = planetFacts;
-    console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
-    return(discoveryYears);
+const question_2 = function() {
+    const result = [...new Set("referee")].join("");
+    return result;
+    //"ref"
 }
 
-/* Object destructuring 3 */
-const objectDestructuring_3 = function() {
-    getUserData = function({firstName, favoriteColor="green"}) {
-        return `Your name is ${firstName} and you like ${favoriteColor}`;
-    }
-    return [
-        getUserData({firstName: "Alejandro", favoriteColor: "purple"}), // 'Your name is Alejandro and you like purple'
-        getUserData({firstName: "Melissa"}), // 'Your name is Melissa and you like green'
-        getUserData({}) // 'Your name is undefined and you like green'
-    ];
+const question_3 = function() {
+    let m = new Map();
+    m.set([1,2,3], true);
+    m.set([1,2,3], false);
+    return m;
+    //Map(2) {(3) [1, 2, 3] => true, (3) [1, 2, 3] => false}
 }
 
-/* Array destructuring 1 */
-const arrayDestructuring_1 = function() {
-    let [first, second, third] = ["Maya", "Marisa", "Chi"];
-    console.log(first), // Maya
-    console.log(second), // Marisa
-    console.log(third) // Chi
-    return [first, second, third];
+const hasDuplicate = function(arr) {
+    const set = new Set(arr);
+    return set.size !== arr.length;
 }
 
-/* Array destructuring 2 */
-const arrayDestructuring_2 = function() {
-    let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
-    "Raindrops on roses",
-    "whiskers on kittens",
-    "Bright copper kettles",
-    "warm woolen mittens",
-    "Brown paper packages tied up with strings"
-    ];
-    console.log(raindrops), // Raindrops on roses
-    console.log(whiskers), // whiskers on kittens
-    console.log(aFewOfMyFavoriteThings) // (3) ['Bright copper kettles', 'warm woolen mittens', 'Brown paper packages tied up with strings']
-    return [raindrops, whiskers, aFewOfMyFavoriteThings];
-}
-
-/* Array destructuring 3 */
-const arrayDestructuring_3 = function() {
-    let numbers = [10, 20, 30];
-    [numbers[1], numbers[2]] = [numbers[2], numbers[1]];
-    console.log(numbers); // (3) [10, 30, 20])
-    return numbers;
-}
-
-const assignVarToObj = function(x, y) {
-    //Create an object and destructure the arguments into it (nestedly)
-    let obj = {
-        numbers: {
-          a:x,
-          b:y
+const vowelCount = function(str) {
+    const vowels = new Map([
+        ['a', 0],
+        ['e', 0],
+        ['i', 0],
+        ['o', 0],
+        ['u', 0]
+    ]);
+    const lowStr = str.toLowerCase();
+    for(element of Array.from(lowStr)) {
+        let vowel = vowels.get(element);
+        if(vowel != undefined) {
+            vowels.set(element, ++vowel);
         }
     };
-    const {a, b} = obj.numbers;
-    //For testing purposes we'll return the local variables as an array
-    return [a, b];
+    //Now remove unused vowels
+    vowels.forEach((value, key) => {
+        if(value === 0)vowels.delete(key);
+    });
+    return vowels;
 }
-
-const arraySwap = function() {
-    let arr = [1, 2];
-    //One-line array swap
-    return [arr[0], arr[1]] = [arr[1], arr[0]];   //[2, 1]
-}
-
-const raceResults = ([first, second, third, ...others]) => ({first, second, third, others});
-
 
 
